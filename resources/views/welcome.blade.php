b2887af49cf62cc4ee0a0f2bcf5905cd752f6109
Clyde Dexter Santiago <clydesantiago@MacBook-Pro-M1.local>
2022-12-09 16:29:11 +0800

    Updated README.md

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>How to Install Tailwind CSS 3 in Laravel 9 With Vite 3 - TechvBlogs</title>
+    <title>{{ config('app.name') }}</title>
 
     @vite('resources/js/app.js')
 </head>
